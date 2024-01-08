ee8ecdb0c97caec50df488361f1e0efdcf8cda83
francesmaloney <FrancesMaloney@westat.com>
2024-01-08 10:14:43 -0500

    Added Loghelper folder and file

---
+++ SBMobileMAUI1/SupportingFiles/Logging/LogHelper.cs
@@ -0,0 +1,41 @@
+﻿namespace SBMobileMAUI1;
+
+public class LogHelper
+{
+    public LogHelper()
+    {
+    }
+    public static string GetLogFileFolder()
+    {
+
+        string baseflder = string.Empty;
+
+        if (DeviceInfo.Current.Platform == DevicePlatform.iOS)
+            baseflder = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+        else if (DeviceInfo.Current.Platform == DevicePlatform.Android)
+            baseflder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        else
+            throw new Exception("Could not initialize Logger: Unknown Platform");
+
+        return System.IO.Path.Combine(baseflder, "logs");
+    }
+
+    /// <summary>
+    /// full name path to log file (template)
+    /// </summary>
+    public static string GetLogFilename()
+    {
+        return System.IO.Path.Combine(GetLogFileFolder(), "log.json");
+    }
+
+    /// <summary>
+    /// return arrary of log files
+    /// </summary>
+    /// <returns></returns>
+    public static string[] GetFiles()
+    {
+        var logfilefolder = GetLogFileFolder();
+
+        return System.IO.Directory.GetFiles(logfilefolder, "log*.json");
+    }
+}
