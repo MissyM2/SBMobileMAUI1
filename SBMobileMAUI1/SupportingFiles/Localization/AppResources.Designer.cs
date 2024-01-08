fd555995d7b34cc4d678fb6919b69ae669410a01
francesmaloney <FrancesMaloney@westat.com>
2024-01-08 12:48:58 -0500

    added localization support 2

---
+++ SBMobileMAUI1/SupportingFiles/Localization/AppResources.Designer.cs
@@ -0,0 +1,670 @@
+namespace SBMobileMAUI1.SupportingFiles.Localization
+{
+    using System;
+    using System.Reflection;
+
+
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    internal class AppResources
+    {
+
+        private static System.Resources.ResourceManager resourceMan;
+
+        private static System.Globalization.CultureInfo resourceCulture;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal AppResources()
+        {
+        }
+
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if (object.Equals(null, resourceMan))
+                {
+                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("SBMobileMAUI1.SupportingFiles.Localization.AppResources", typeof(AppResources).Assembly);
+                    resourceMan = temp;
+                }
+                return resourceMan;
+            }
+        }
+
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static System.Globalization.CultureInfo Culture
+        {
+            get
+            {
+                return resourceCulture;
+            }
+            set
+            {
+                resourceCulture = value;
+            }
+        }
+
+        // General
+
+        public static string ChangeLanguage
+        {
+            get
+            {
+                return ResourceManager.GetString("ChangeLanguage", resourceCulture);
+            }
+        }
+
+        public static string CurrLanguage
+        {
+            get
+            {
+                return ResourceManager.GetString("CurrLanguage", resourceCulture);
+            }
+        }
+
+        public static string English
+        {
+            get
+            {
+                return ResourceManager.GetString("English", resourceCulture);
+            }
+        }
+
+        public static string Spanish
+        {
+            get
+            {
+                return ResourceManager.GetString("Spanish", resourceCulture);
+            }
+        }
+
+        public static string Save
+        {
+            get
+            {
+                return ResourceManager.GetString("Save", resourceCulture);
+            }
+        }
+
+        public static string AppDescription
+        {
+            get
+            {
+                return ResourceManager.GetString("AppDescription", resourceCulture);
+            }
+        }
+
+        public static string Version
+        {
+            get
+            {
+                return ResourceManager.GetString("Version", resourceCulture);
+            }
+        }
+
+        public static string LoginText
+        {
+            get
+            {
+                return ResourceManager.GetString("LoginText", resourceCulture);
+            }
+        }
+
+        public static string UserNameString
+        {
+            get
+            {
+                return ResourceManager.GetString("UserNameString", resourceCulture);
+            }
+        }
+
+        public static string PasswordString
+        {
+            get
+            {
+                return ResourceManager.GetString("PasswordString", resourceCulture);
+            }
+        }
+
+        public static string PaperworkTitle
+        {
+            get
+            {
+                return ResourceManager.GetString("PaperworkTitle", resourceCulture);
+            }
+        }
+
+        public static string PaperworkBody
+        {
+            get
+            {
+                return ResourceManager.GetString("PaperworkBody", resourceCulture);
+            }
+        }
+        public static string UserNamePlaceholder
+        {
+            get
+            {
+                return ResourceManager.GetString("UserNamePlaceholder", resourceCulture);
+            }
+        }
+
+        public static string PasswordPlaceholder
+        {
+            get
+            {
+                return ResourceManager.GetString("PasswordPlaceholder", resourceCulture);
+            }
+        }
+
+        public static string LoginButton
+        {
+            get
+            {
+                return ResourceManager.GetString("LoginButton", resourceCulture);
+            }
+        }
+
+        public static string Authenticating
+        {
+            get
+            {
+                return ResourceManager.GetString("Authenticating", resourceCulture);
+            }
+        }
+
+        // Flyout Menu
+
+        public static string About
+        {
+            get
+            {
+                return ResourceManager.GetString("About", resourceCulture);
+            }
+        }
+
+        public static string FAQ
+        {
+            get
+            {
+                return ResourceManager.GetString("FAQ", resourceCulture);
+            }
+        }
+
+        public static string Resources
+        {
+            get
+            {
+                return ResourceManager.GetString("Resources", resourceCulture);
+            }
+        }
+
+        public static string Settings
+        {
+            get
+            {
+                return ResourceManager.GetString("Settings", resourceCulture);
+            }
+        }
+
+
+        public static string IncentivesMenuItem
+        {
+            get
+            {
+                return ResourceManager.GetString("IncentivesMenuItem", resourceCulture);
+            }
+        }
+
+        public static string IncentivesView
+        {
+            get
+            {
+                return ResourceManager.GetString("IncentivesView", resourceCulture);
+            }
+        }
+
+        public static string ContactMenuItem
+        {
+            get
+            {
+                return ResourceManager.GetString("ContactMenuItem", resourceCulture);
+            }
+        }
+
+        public static string SignOutString
+        {
+            get
+            {
+                return ResourceManager.GetString("SignOutString", resourceCulture);
+            }
+        }
+        // Survey List Page
+
+        public static string LoadingSurveys
+        {
+            get
+            {
+                return ResourceManager.GetString("LoadingSurveys", resourceCulture);
+            }
+        }
+
+        internal static string BeginSurvey
+        {
+            get
+            {
+                return ResourceManager.GetString("BeginSurvey", resourceCulture);
+            }
+        }
+
+        internal static string BeginCurrSurvey
+        {
+            get
+            {
+                return ResourceManager.GetString("BeginCurrSurvey", resourceCulture);
+            }
+        }
+
+        public static string SingleEventSurvey
+        {
+            get
+            {
+                return ResourceManager.GetString("SingleEventSurvey", resourceCulture);
+            }
+        }
+        public static string EndofDaySurvey
+        {
+            get
+            {
+                return ResourceManager.GetString("EndofDaySurvey", resourceCulture);
+            }
+        }
+
+        public static string UncompletedSurveys
+        {
+            get
+            {
+                return ResourceManager.GetString("UncompletedSurveys", resourceCulture);
+            }
+        }
+
+        public static string NoUncompletedSurveys
+        {
+            get
+            {
+                return ResourceManager.GetString("NoUncompletedSurveys", resourceCulture);
+            }
+        }
+
+        public static string HasNoSurveys
+        {
+            get
+            {
+                return ResourceManager.GetString("HasNoSurveys", resourceCulture);
+            }
+        }
+
+        public static string HasNoSurveysDesc
+        {
+            get
+            {
+                return ResourceManager.GetString("HasNoSurveysDesc", resourceCulture);
+            }
+        }
+
+        // Messages
+
+        public static string LoginActivityIndicator
+        {
+            get
+            {
+                return ResourceManager.GetString("LoginActivityIndicator", resourceCulture);
+            }
+        }
+
+        public static string AccountLocked
+        {
+            get
+            {
+                return ResourceManager.GetString("AccountLocked", resourceCulture);
+            }
+        }
+
+        public static string AccountDisabled
+        {
+            get
+            {
+                return ResourceManager.GetString("AccountDisabled", resourceCulture);
+            }
+        }
+
+        public static string EnterCreds
+        {
+            get
+            {
+                return ResourceManager.GetString("EnterCreds", resourceCulture);
+            }
+        }
+
+        public static string IncorrectCreds
+        {
+            get
+            {
+                return ResourceManager.GetString("IncorrectCreds", resourceCulture);
+            }
+        }
+
+        public static string ErrorHeader
+        {
+            get
+            {
+                return ResourceManager.GetString("ErrorHeader", resourceCulture);
+            }
+        }
+
+
+
+        // Other
+
+        internal static string PushDatatoServer
+        {
+            get
+            {
+                return ResourceManager.GetString("PushDatatoServer", resourceCulture);
+            }
+        }
+
+        internal static string SignOut
+        {
+            get
+            {
+                return ResourceManager.GetString("SignOut", resourceCulture);
+            }
+        }
+
+        // Days of Week
+
+        internal static string SundayDOW
+        {
+            get
+            {
+                return ResourceManager.GetString("SundayDOW", resourceCulture);
+            }
+        }
+
+        internal static string MondayDOW
+        {
+            get
+            {
+                return ResourceManager.GetString("MondayDOW", resourceCulture);
+            }
+        }
+
+        internal static string TuesdayDOW
+        {
+            get
+            {
+                return ResourceManager.GetString("TuesdayDOW", resourceCulture);
+            }
+        }
+
+        internal static string WednesdayDOW
+        {
+            get
+            {
+                return ResourceManager.GetString("WednesdayDOW", resourceCulture);
+            }
+        }
+
+        internal static string ThursdayDOW
+        {
+            get
+            {
+                return ResourceManager.GetString("ThursdayDOW", resourceCulture);
+            }
+        }
+
+        internal static string FridayDOW
+        {
+            get
+            {
+                return ResourceManager.GetString("FridayDOW", resourceCulture);
+            }
+        }
+
+        internal static string SaturdayDOW
+        {
+            get
+            {
+                return ResourceManager.GetString("SaturdayDOW", resourceCulture);
+            }
+        }
+        internal static string JanString
+        {
+            get
+            {
+                return ResourceManager.GetString("JanString", resourceCulture);
+            }
+        }
+        internal static string FebString
+        {
+            get
+            {
+                return ResourceManager.GetString("FebString", resourceCulture);
+            }
+        }
+        internal static string MarString
+        {
+            get
+            {
+                return ResourceManager.GetString("MarString", resourceCulture);
+            }
+        }
+        internal static string AprString
+        {
+            get
+            {
+                return ResourceManager.GetString("AprString", resourceCulture);
+            }
+        }
+        internal static string MayString
+        {
+            get
+            {
+                return ResourceManager.GetString("MayString", resourceCulture);
+            }
+        }
+        internal static string JunString
+        {
+            get
+            {
+                return ResourceManager.GetString("JunString", resourceCulture);
+            }
+        }
+        internal static string JulString
+        {
+            get
+            {
+                return ResourceManager.GetString("JulString", resourceCulture);
+            }
+        }
+        internal static string AugString
+        {
+            get
+            {
+                return ResourceManager.GetString("AugString", resourceCulture);
+            }
+        }
+        internal static string SeptString
+        {
+            get
+            {
+                return ResourceManager.GetString("SeptString", resourceCulture);
+            }
+        }
+        internal static string OctString
+        {
+            get
+            {
+                return ResourceManager.GetString("OctString", resourceCulture);
+            }
+        }
+        internal static string NovString
+        {
+            get
+            {
+                return ResourceManager.GetString("NovString", resourceCulture);
+            }
+        }
+        internal static string DecString
+        {
+            get
+            {
+                return ResourceManager.GetString("DecString", resourceCulture);
+            }
+        }
+
+        // Instrument
+
+        internal static string SaveUntilLaterString
+        {
+            get
+            {
+                return ResourceManager.GetString("SaveUntilLaterString", resourceCulture);
+            }
+        }
+
+        internal static string BackString
+        {
+            get
+            {
+                return ResourceManager.GetString("BackString", resourceCulture);
+            }
+        }
+
+        internal static string NextString
+        {
+            get
+            {
+                return ResourceManager.GetString("NextString", resourceCulture);
+            }
+        }
+
+        internal static string SubmitString
+        {
+            get
+            {
+                return ResourceManager.GetString("SubmitString", resourceCulture);
+            }
+        }
+
+        internal static string Today
+        {
+            get
+            {
+                return ResourceManager.GetString("Today", resourceCulture);
+            }
+        }
+
+        // Rewards
+
+        internal static string Rewards
+        {
+            get
+            {
+                return ResourceManager.GetString("Rewards", resourceCulture);
+            }
+        }
+
+        internal static string Total
+        {
+            get
+            {
+                return ResourceManager.GetString("Total", resourceCulture);
+            }
+        }
+
+        internal static string TotalUpdated
+        {
+            get
+            {
+                return ResourceManager.GetString("TotalUpdated", resourceCulture);
+            }
+        }
+
+        // map control
+
+        internal static string ConfirmLocation
+        {
+            get
+            {
+                return ResourceManager.GetString("ConfirmLocation", resourceCulture);
+            }
+        }
+
+        internal static string ConfirmLocationInMap
+        {
+            get
+            {
+                return ResourceManager.GetString("ConfirmLocationInMap", resourceCulture);
+            }
+        }
+
+        internal static string SearchBarPlaceholder
+        {
+            get
+            {
+                return ResourceManager.GetString("SearchBarPlaceholder", resourceCulture);
+            }
+        }
+
+        internal static string SelectYourLocation
+        {
+            get
+            {
+                return ResourceManager.GetString("SelectYourLocation", resourceCulture);
+            }
+        }
+
+        internal static string CommunitySurvey
+        {
+            get
+            {
+                return ResourceManager.GetString("CommunitySurvey", resourceCulture);
+            }
+        }
+
+        internal static string CommunityString
+        {
+            get
+            {
+                return ResourceManager.GetString("CommunityString", resourceCulture);
+            }
+        }
+
+        internal static string SurveyString
+        {
+            get
+            {
+                return ResourceManager.GetString("SurveyString", resourceCulture);
+            }
+        }
+        public static string AlertOk
+        {
+            get
+            {
+                return ResourceManager.GetString("AlertOk", resourceCulture);
+            }
+        }
+        public static string SurveyCompletionAlert
+        {
+            get
+            {
+                return ResourceManager.GetString("SurveyCompletionAlert", resourceCulture);
+            }
+        }
+
+    }
+}
